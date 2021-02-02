453816b8895933ff94df4ec93cbb6f05b2994254
miloszcwen <cwen.milosz@gmail.com>
2021-02-02 23:36:06 +0100

    remove console logs

--- src/js/preload.js
+++ src/js/preload.js
@@ -6,14 +6,15 @@ fetch('https://protected-taiga-19734.herokuapp.com/signin', {
         email: "a",
         password: "a"
     })
-}).then(response=>response.json())
-.then(null)
-.catch(err1=>console.log(err1))
+    })
+    .then(response=>response.json())
+    .then(null)
+    .catch(err1)
 
 fetch('https://miloszcwen.github.io/fans/')
-        .then((response) => response.json())
-        .then(null)
-        .catch(err2=>console.log(err2))
-};
+    .then((response) => response.json())
+    .then(null)
+    .catch(err2)
+}
 
 export default preload;
