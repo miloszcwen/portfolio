e1f2ef5b866ffc5bb24d3b608d23dda0d6f2fa7e
miloszcwen <cwen.milosz@gmail.com>
2021-02-02 22:24:56 +0100

    add script to preload heroku

---
+++ src/js/preload.js
@@ -0,0 +1,14 @@
+console.log("start")
+etch('https://protected-taiga-19734.herokuapp.com/signin', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+        email: "a",
+        password: "a"
+    })
+}).then(response=>response.json())
+.then(user=>{
+    console.log(user)
+    }
+)
+.catch(err=>console.log(err))
