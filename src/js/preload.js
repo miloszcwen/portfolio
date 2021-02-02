967d34eb65a6b2e53aa43c503cd72528f18d5458
miloszcwen <cwen.milosz@gmail.com>
2021-02-02 22:54:39 +0100

    add second fetch in preload.js

--- src/js/preload.js
+++ src/js/preload.js
@@ -8,12 +8,12 @@ fetch('https://protected-taiga-19734.herokuapp.com/signin', {
     })
 }).then(response=>response.json())
 .then(user=>console.log(user))
-.catch(err=>console.log(err))
+.catch(erra=>console.log(erra))
 
 fetch('https://miloszcwen.github.io/fans/')
         .then((response) => response.json())
         .then((users) => console.log(users))
-        .catch(err=>console.log(err))
+        .catch(erro=>console.log(erro))
 };
 
 export default preload;
